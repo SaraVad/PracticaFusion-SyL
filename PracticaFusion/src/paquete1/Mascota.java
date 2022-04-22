da829e387b9d74c49b3547064ba015d9cdf4b850
leo <leo@gmail.com>
2022-04-22 16:07:29 +0200

    Modificacion de la clase Mascota y actualizacion del metodo mardarMensaje

--- PracticaFusion/src/paquete1/Mascota.java
+++ PracticaFusion/src/paquete1/Mascota.java
@@ -1,7 +1,9 @@
 package paquete1;
 
 public class Mascota {
+
 	private String nombre;
+	private String duenyo;
 
 	public Mascota(String nombre) {
 		this.nombre = nombre;
@@ -15,8 +17,16 @@ public class Mascota {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	public String getDuenyo() {
+		return duenyo;
+	}
+
+	public void setDuenyo(String duenyo) {
+		this.duenyo = duenyo;
+	}
 
 	public void mandarMensaje() {
-		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no, mi duenyo es " + getDuenyo());
 	}
 }
